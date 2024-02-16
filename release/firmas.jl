9290b8860bc46da065643027349bd091d13fc289
OscarUDC <oscar.vilela.rodriguez@udc.es>
2024-02-16 14:46:21 +0100

    refactor: :recycle: Add release folder for the final code for delivery

---
+++ release/firmas.jl
@@ -0,0 +1,317 @@
+
+# Tened en cuenta que en este archivo todas las funciones tienen puesta la palabra reservada 'function' y 'end' al final
+# Según cómo las defináis, podrían tener que llevarlas o no
+
+# ----------------------------------------------------------------------------------------------
+# ------------------------------------- Practica 2 ---------------------------------------------
+# ----------------------------------------------------------------------------------------------
+
+using Statistics
+using Flux
+using Flux.Losses
+
+
+# -------------------------------------------------------------------------
+# Funciones para codificar entradas y salidas categóricas
+
+# Funcion para realizar la codificacion, recibe el vector de caracteristicas (uno por patron), y las clases
+function oneHotEncoding(feature::AbstractArray{<:Any,1}, classes::AbstractArray{<:Any,1})
+    #
+    # Codigo a desarrollar
+    #
+end;
+# Esta funcion es similar a la anterior, pero si no es especifican las clases, se toman de la propia variable
+function oneHotEncoding(feature::AbstractArray{<:Any,1})
+    #
+    # Codigo a desarrollar
+    #
+end;
+# Sobrecargamos la funcion oneHotEncoding por si acaso pasan un vector de valores booleanos
+#  En este caso, el propio vector ya está codificado, simplemente lo convertimos a una matriz columna
+function oneHotEncoding(feature::AbstractArray{Bool,1})
+    #
+    # Codigo a desarrollar
+    #
+end;
+# Cuando se llame a la funcion oneHotEncoding, según el tipo del argumento pasado, Julia realizará
+#  la llamada a la función correspondiente
+
+
+# -------------------------------------------------------------------------
+# Funciones para calcular los parametros de normalizacion y normalizar
+
+# Para calcular los parametros de normalizacion, segun la forma de normalizar que se desee:
+function calculateMinMaxNormalizationParameters(dataset::AbstractArray{<:Real,2})
+    #
+    # Codigo a desarrollar
+    #
+end;
+function calculateZeroMeanNormalizationParameters(dataset::AbstractArray{<:Real,2})
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+# 4 versiones de la funcion para normalizar entre 0 y 1:
+#  - Nos dan los parametros de normalizacion, y se quiere modificar el array de entradas (el nombre de la funcion acaba en '!')
+#  - No nos dan los parametros de normalizacion, y se quiere modificar el array de entradas (el nombre de la funcion acaba en '!')
+#  - Nos dan los parametros de normalizacion, y no se quiere modificar el array de entradas (se crea uno nuevo)
+#  - No nos dan los parametros de normalizacion, y no se quiere modificar el array de entradas (se crea uno nuevo)
+function normalizeMinMax!(dataset::AbstractArray{<:Real,2}, normalizationParameters::NTuple{2, AbstractArray{<:Real,2}})
+    #
+    # Codigo a desarrollar
+    #
+end;
+function normalizeMinMax!(dataset::AbstractArray{<:Real,2})
+    #
+    # Codigo a desarrollar
+    #
+end;
+function normalizeMinMax( dataset::AbstractArray{<:Real,2}, normalizationParameters::NTuple{2, AbstractArray{<:Real,2}})
+    #
+    # Codigo a desarrollar
+    #
+end;
+function normalizeMinMax( dataset::AbstractArray{<:Real,2})
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+# 4 versiones similares de la funcion para normalizar de media 0:
+#  - Nos dan los parametros de normalizacion, y se quiere modificar el array de entradas (el nombre de la funcion acaba en '!')
+#  - No nos dan los parametros de normalizacion, y se quiere modificar el array de entradas (el nombre de la funcion acaba en '!')
+#  - Nos dan los parametros de normalizacion, y no se quiere modificar el array de entradas (se crea uno nuevo)
+#  - No nos dan los parametros de normalizacion, y no se quiere modificar el array de entradas (se crea uno nuevo)
+function normalizeZeroMean!(dataset::AbstractArray{<:Real,2}, normalizationParameters::NTuple{2, AbstractArray{<:Real,2}})
+    #
+    # Codigo a desarrollar
+    #
+end;
+function normalizeZeroMean!(dataset::AbstractArray{<:Real,2})
+    #
+    # Codigo a desarrollar
+    #
+end;
+function normalizeZeroMean( dataset::AbstractArray{<:Real,2}, normalizationParameters::NTuple{2, AbstractArray{<:Real,2}})
+    #
+    # Codigo a desarrollar
+    #
+end;
+function normalizeZeroMean( dataset::AbstractArray{<:Real,2})
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+
+# -------------------------------------------------------
+# Funcion que permite transformar una matriz de valores reales con las salidas del clasificador o clasificadores en una matriz de valores booleanos con la clase en la que sera clasificada
+
+function classifyOutputs(outputs::AbstractArray{<:Real,1}; threshold::Real=0.5)
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+function classifyOutputs(outputs::AbstractArray{<:Real,2}; threshold::Real=0.5)
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+
+# -------------------------------------------------------
+# Funciones para calcular la precision
+
+function accuracy(outputs::AbstractArray{Bool,1}, targets::AbstractArray{Bool,1})
+    #
+    # Codigo a desarrollar
+    #
+end;
+function accuracy(outputs::AbstractArray{Bool,2}, targets::AbstractArray{Bool,2})
+    #
+    # Codigo a desarrollar
+    #
+end;
+function accuracy(outputs::AbstractArray{<:Real,1}, targets::AbstractArray{Bool,1}; threshold::Real=0.5)
+    #
+    # Codigo a desarrollar
+    #
+end;
+function accuracy(outputs::AbstractArray{<:Real,2}, targets::AbstractArray{Bool,2}; threshold::Real=0.5)
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+# -------------------------------------------------------
+# Funciones para crear y entrenar una RNA
+function buildClassANN(numInputs::Int, topology::AbstractArray{<:Int,1}, numOutputs::Int; transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)))
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+function trainClassANN(topology::AbstractArray{<:Int,1}, dataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}; transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)), maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01)
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+function trainClassANN(topology::AbstractArray{<:Int,1}, (inputs, targets)::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}}; transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)), maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01)
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+
+# ----------------------------------------------------------------------------------------------
+# ------------------------------------- Practica 3 ---------------------------------------------
+# ----------------------------------------------------------------------------------------------
+
+using Random
+
+function holdOut(N::Int, P::Real)
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+function holdOut(N::Int, Pval::Real, Ptest::Real)
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+
+# Funcion para entrenar RR.NN.AA. con conjuntos de entrenamiento, validacion y test. Estos dos ultimos son opcionales
+# Es la funcion anterior, modificada para calcular errores en los conjuntos de validacion y test y realizar parada temprana si es necesario
+function trainClassANN(topology::AbstractArray{<:Int,1},
+    trainingDataset::  Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}};
+    validationDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}=(Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0)),
+    testDataset::      Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}=(Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0)),
+    transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)),
+    maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01, maxEpochsVal::Int=20)
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+
+function trainClassANN(topology::AbstractArray{<:Int,1},
+    trainingDataset::  Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}};
+    validationDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}}=(Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0)),
+    testDataset::      Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}}=(Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0)),
+    transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)),
+    maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01, maxEpochsVal::Int=20)
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+
+
+# ----------------------------------------------------------------------------------------------
+# ------------------------------------- Practica 4 ---------------------------------------------
+# ----------------------------------------------------------------------------------------------
+
+
+
+function confusionMatrix(outputs::AbstractArray{Bool,1}, targets::AbstractArray{Bool,1})
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+function confusionMatrix(outputs::AbstractArray{<:Real,1}, targets::AbstractArray{Bool,1}; threshold::Real=0.5)
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+function confusionMatrix(outputs::AbstractArray{Bool,2}, targets::AbstractArray{Bool,2}; weighted::Bool=true)
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+function confusionMatrix(outputs::AbstractArray{<:Real,2}, targets::AbstractArray{Bool,2}; weighted::Bool=true)
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+function confusionMatrix(outputs::AbstractArray{<:Any,1}, targets::AbstractArray{<:Any,1}; weighted::Bool=true)
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+
+
+# ----------------------------------------------------------------------------------------------
+# ------------------------------------- Practica 5 ---------------------------------------------
+# ----------------------------------------------------------------------------------------------
+
+using Random
+using Random:seed!
+
+function crossvalidation(N::Int64, k::Int64)
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+function crossvalidation(targets::AbstractArray{Bool,1}, k::Int64)
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+function crossvalidation(targets::AbstractArray{Bool,2}, k::Int64)
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+function crossvalidation(targets::AbstractArray{<:Any,1}, k::Int64)
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+
+
+
+
+
+
+function ANNCrossValidation(topology::AbstractArray{<:Int,1},
+    inputs::AbstractArray{<:Real,2}, targets::AbstractArray{<:Any,1},
+    crossValidationIndices::Array{Int64,1};
+    numExecutions::Int=50,
+    transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)),
+    maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01, validationRatio::Real=0, maxEpochsVal::Int=20, showText::Bool=false)
+    #
+    # Codigo a desarrollar
+    #
+end;
+
+
+# ----------------------------------------------------------------------------------------------
+# ------------------------------------- Practica 6 ---------------------------------------------
+# ----------------------------------------------------------------------------------------------
+
+using ScikitLearn: @sk_import, fit!, predict
+
+@sk_import svm: SVC
+@sk_import tree: DecisionTreeClassifier
+@sk_import neighbors: KNeighborsClassifier
+
+
+function modelCrossValidation(modelType::Symbol, modelHyperparameters::Dict, inputs::AbstractArray{<:Real,2}, targets::AbstractArray{<:Any,1}, crossValidationIndices::Array{Int64,1})
+    #
+    # Codigo a desarrollar
+    #
+end;
