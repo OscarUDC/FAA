693027888e8eeafffa4cba849259a7fa214b1808
iago Grandal <i.gdelrio@udc.es>
2024-02-21 23:52:28 +0100

    the other trainClassANN function

--- src/06_neuralnetwork_training.jl
+++ src/06_neuralnetwork_training.jl
@@ -37,3 +37,27 @@ function trainClassANN(topology::AbstractArray{<:Int,1},
     end;
 
 end
+
+function trainClassANN(topology::AbstractArray{<:Int,1},
+     dataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}},
+    transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)),
+    maxEpochs::Int=1000,
+    minLoss::Real=0.0,
+    learningRate::Real=0.01)
+
+    inputs = dataset[0]
+    targets = dataset[1]
+    inputsT = transpose(inputs);
+    targetsT = transpose(targets);
+    ann = buildClassANN(size(inputs, 1), topology, size(targets, 1), transferFunctions);
+
+    loss(model, inputs, targets) = size(targets,1) ? Losses.binaryCrossEntropy(model(inputs), targets): Losses.crossEntropy(model(inputs), targets);
+    opt_state = Flux.setup(Adam(learningRate), ann);
+    for epoch in 0:1:maxEpochs
+        Flux.train!(loss, ann, [(inputsT, targetsT)], opt_state);
+        if loss(ann, inputsT, targetsT) <= minLoss
+            return ann;
+        end;
+        return ann;
+    end;
+end;
