2fc8bddde0602e7f0ad152b837ba52028d5ae9c4
Hugo Bouzon <hugo.bouzon.fernandez@udc.es>
2024-02-22 16:31:16 +0100

    Fixed "classification.jl".

--- src/03_classification.jl
+++ src/03_classification.jl
@@ -4,7 +4,12 @@ end;
 
 function classifyOutputs(outputs::AbstractArray{<:Real,2}; threshold::Real=0.5)
     if size(outputs, 2) == 1
-        rowwise_output = classifyOutputs(outputs[:]; threshold)
-        return reshape(rowwise_output, :, 1)
+        rowwise_outputs = classifyOutputs(outputs[:]; threshold)
+        return reshape(rowwise_outputs, :, 1)
+    else
+        (_, indicesMaxEachInstance) = findmax(outputs, dims = 2)
+        outputs = falses(size(outputs))
+        outputs[indicesMaxEachInstance] .= true
+        return outputs
     end
 end;
