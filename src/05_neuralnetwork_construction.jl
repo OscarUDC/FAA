b46a8d9a1a8e0bb4efe8207aeb5c47954fb02597
IagoGrandal <i.gdelrio@udc.es>
2024-03-19 19:20:21 +0100

    Finishing debugging construction of ANN and training

--- src/05_neuralnetwork_construction.jl
+++ src/05_neuralnetwork_construction.jl
@@ -1,26 +1,28 @@
 using Flux
 
 
+"""
+Creates a neuronal network with the depth chosen, and with the number of neurons chosen
+----------
+Attributes
+----------
+numInputs: number of inputs the neuronal network will recieve.
+topology: number of neurones each layer has. 
+numOutputs: number of outputs the neuronal network has.
+transferFunctions: which transfer fuction each layer has.
+"""
 function buildClassANN(numInputs::Int, topology::AbstractArray{<:Int,1}, numOutputs::Int,  
     transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology))) 
-    """
-    Creates a neuronal network with the depth chosen, and with the number of neurons chosen
-    ----------
-    Attributes
-    ----------
-    numInputs: number of inputs the neuronal network will recieve.
-    topology: number of neurones each layer has. 
-    numOutputs: number of outputs the neuronal network has.
-    transferFunctions: which transfer fuction each layer has.
-    """
-    ann = Chain();                                                                                          #initializes the ANN
-    ann = Chain(ann..., Dense(numInputs, topology[0],transferFunctions[0]));                                #first layer of the ANN
-    number_before = 0;
-for number in 1:1:(length(topology) - 1)                                                                         #loop that will create
-        ann = Chain(ann..., Dense(topology[number_before], topology[number],transferFunctions[number]));    #the other layers of the ANN
-        number_before = number;
+   
+    ann = Chain()                                                                                           #initializes the ANN
+    ann = Chain(ann..., Dense(numInputs, topology[1],transferFunctions[1]))                                 #first layer of the ANN
+    number_before = 1
+   
+    for number in 2:(length(topology) - 1)                                                                    #loop that will create
+        ann = Chain(ann..., Dense(topology[number_before], topology[number],transferFunctions[number]))     #the other layers of the ANN
+        number_before = number 
     end
-    ann = Chain(ann..., Dense(topology[number_before], numOutputs, transferFunctions[number_before]));      #last layer of the ANN
-    ann = Chain(ann..., softmax);                                                                           #the softmax function
-    return ann;
+    ann = Chain(ann..., Dense(topology[number_before], numOutputs, identity))                               #last layer of the ANN
+    ann = Chain(ann..., softmax)                                                                            #the softmax function
+    return ann
 end
--- src/06_neuralnetwork_training.jl
+++ src/06_neuralnetwork_training.jl
@@ -21,10 +21,10 @@ function trainClassANN(topology::AbstractArray{<:Int,1},
     minLoss::Real=0.0, 
     learningRate::Real=0.01)
 
-    newTargets = reshape(targets, :, size(targets));
+    newTargets = reshape(targets, :, size(targets))
     
-    trainClassANN(topology, Tuple{inputs, newTargets}, transferFunctions, maxEpochs, minLoss, learningRate);
-end;
+    trainClassANN(topology, Tuple{inputs, newTargets}, transferFunctions, maxEpochs, minLoss, learningRate)
+end
 
 """
 Creation of the loss function, please the matrix has to have arranged the patrons in colums
@@ -35,7 +35,7 @@ model: model to calculate the loss
 inputs: the inputs you'll use to calculate the loss
 targets: the targets youll use to calculate the loss        
 """
-loss(model, inputs, targets) = size(targets,1) ? Losses.binaryCrossEntropy(model(inputs), targets) : Losses.crossEntropy(model(inputs), targetsT);
+loss(model, inputs, targets) = size(targets,1) ? Losses.binaryCrossEntropy(model(inputs), targets) : Losses.crossEntropy(model(inputs), targetsT)
 
 
 """
@@ -60,16 +60,16 @@ function trainClassANN(topology::AbstractArray{<:Int,1},
 
     inputs = dataset[0]
     targets = dataset[1]
-    inputsT = transpose(inputs);
-    targetsT = transpose(targets);
-    ann = buildClassANN(numInputs=size(inputs, 1), topology, numOutputs=size(targets, 1), transferFunctions);
+    inputsT = transpose(inputs)
+    targetsT = transpose(targets)
+    ann = buildClassANN(numInputs=size(inputs, 1), topology, numOutputs=size(targets, 1), transferFunctions)
 
-    opt_state = Flux.setup(Adam(learningRate), ann);
-    for epoch in 0:1:maxEpochs
-        Flux.train!(loss, ann, [(inputsT, targetsT)], opt_state);
+    opt_state = Flux.setup(Adam(learningRate), ann)
+    for epoch in 1:maxEpochs
+        Flux.train!(loss, ann, [(inputsT, targetsT)], opt_state)
         if loss(ann, inputsT, targetsT) <= minLoss
-            return ann;
-        end;
-    return ann;
-    end;
-end;
+            return ann
+        end
+    end
+    return ann
+end
