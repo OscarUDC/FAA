1702b272742ac7b8f834f90594eb7cbc11bd7f9b
Hugo Bouzon <hugo.bouzon.fernandez@udc.es>
2024-04-04 21:56:23 +0200

    tried to fix all trainClassANN

--- src/06_neuralnetwork_training.jl
+++ src/06_neuralnetwork_training.jl
@@ -32,12 +32,15 @@ function trainClassANN(topology::AbstractArray{<:Int,1},
     trainClassANN(topology, Tuple{inputs, columnTargets}, transferFunctions, maxEpochs, minLoss, learningRate)
 end
 
-function trainClassANN(topology::AbstractArray{<:Int,1},
-    trainingDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}};
-    validationDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}=(Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0)),
-    testDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}=(Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0)),
-    transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)),
-    maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01, maxEpochsVal::Int=20) 
+function trainClassANN(topology::AbstractArray{<:Int,1}, 
+    trainingDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}; 
+    validationDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}= 
+    (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0)), 
+    testDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}= 
+    (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0)), 
+    transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)), 
+    maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01, 
+    maxEpochsVal::Int=20)
     
     trainingInputs, trainingTargets = trainingDataset
     testInputs, testTargets = testDataset
@@ -48,66 +51,51 @@ function trainClassANN(topology::AbstractArray{<:Int,1},
     testTargets = Float32.(testTargets)
     validationInputs = Float32.(validationInputs)
     validationTargets = Float32.(validationTargets)
+    existsTestDataset = testDataset != (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0))
+    existsValidationDataset = validationDataset != (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0))
 
-    ann = buildClassANN(Int64(size(trainingInputs, 1)),topology, Int64(size(trainingTargets, 1)))
-
-    # Definimos la función de pérdida
-    loss(model, inputs, targets) = size(targets,1)==1 ? Flux.Losses.binarycrossentropy(model(inputs), targets) : Flux.Losses.crossentropy(model(inputs), targets)
-
-    # Inicializamos el optimizador
+    ann = buildClassANN(Int64(size(trainingInputs, 1)),topology, Int64(size(trainingTargets, 1));
+    transferFunctions = transferFunctions)
+    loss(model, x, y) = (size(y, 1) == 1) ? Losses.binarycrossentropy(model(x), y) : Losses.crossentropy(model(x), y)
     opt = Flux.setup(Adam(learningRate), ann)
 
-    # Vector para almacenar los valores de pérdida en cada época
-    lossHistoryTr = Float32[]
-    lossHistoryV = Float32[]
-    lossHistoryTe = Float32[]
+    trainingLossHistory = Float32[]
+    testLossHistory = Float32[]
+    validationLossHistory = Float32[]
 
-    mejorRed = deepcopy(ann)
-    errorMejorRed = Inf
-    ciclos = 0
-    # Bucle de entrenamiento
-    for epoch in 0:maxEpochs  
-        # Calculamos el valor de pérdida en cada época
-        currentLossTr = loss(ann, inputsTr, targetsTr)
-        push!(lossHistoryTr, currentLossTr)
-        
-        if testDataset != (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0))
-            currentLossTe = loss(ann, inputsTe, targetsTe)
-            push!(lossHistoryTe, currentLossTe)
+    bestANN = deepcopy(ann)
+    bestANNError = Inf
+    epochs = 0
+    epoch = 0
+    trainingLoss = minLoss + 1
+    while !(epoch == maxEpochs) || !(epochs == maxEpochsVal) || !(trainingLoss <= minLoss)
+        Flux.train!(loss, Flux.params(ann), [(trainingInputs', trainingTargets')], opt)
+        trainingLoss = loss(ann, trainingInputs', trainingTargets')
+        push!(trainingLossHistory, loss(ann, trainingInputs', trainingTargets'))
+        if existsTestDataset
+            push!(testLossHistory, loss(ann, testInputs', testTargets'))
         end
-
-        Flux.train!(loss, Flux.params(ann), [(inputsTr, targetsTr)], opt)
-
-        if validationDataset != (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0))
-            currentLossV = loss(ann, inputsV, targetsV)
-            push!(lossHistoryV, currentLossV)
-            if currentLossV < errorMejorRed
-                mejorRed = deepcopy(ann)
-                errorMejorRed = currentLossV
-                ciclos = 0
+        if existsValidationDataset
+            validationLoss = loss(ann, validationInputs', validationTargets')
+            push!(validationLossHistory, validationLoss)
+            if validationLoss < bestANNError
+                bestANN = deepcopy(ann)
+                bestANNError = validationLoss
+                epochs = 0
             else 
-                ciclos += 1
-                if ciclos == maxEpochsVal
-                    break
-                end 
+                epochs += 1
             end
         end
-        # Verificamos los criterios de parada
-        if currentLossTr ≤ minLoss
-            println("Entrenamiento completado en la época ", epoch)
-            break
-        end
+        epoch += 1
     end
-
-    # devolver unos valores u otros en funcion de los parametros pasados
-    if testDataset == validationDataset == (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0))
-        return ann, lossHistoryTr
-    elseif testDataset != (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0)) && validationDataset == (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0))
-        return ann, lossHistoryTr, lossHistoryTe
-    elseif  validationDataset != (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0)) && testDataset == (Array{eltype(trainingDataset[1]),2}(undef,0,0), falses(0,0))
-        return mejorRed, lossHistoryTr, lossHistoryV
+    if existsTestDataset & existsValidationDataset
+        return ann, trainingLossHistory
+    elseif existsTestDataset
+        return ann, trainingLossHistory, testLossHistory
+    elseif  existsValidationDataset
+        return bestANN, trainingLossHistory, validationLossHistory
     else 
-        return mejorRed, lossHistoryTr, lossHistoryV, lossHistoryTe
+        return bestANN, trainingLossHistory, validationLossHistory, testLossHistory
     end       
 end
 
@@ -121,15 +109,16 @@ function trainClassANN(topology::AbstractArray{<:Int,1},
     maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01,
     maxEpochsVal::Int=20)
 
-    inputs_train, targets_train = trainingDataset
-    inputs_val, targets_val = validationDataset
-    inputs_test, targets_test = testDataset
-    
-    # Convertir las salidas deseadas de matrices a vectores
-    targets_train = reshape(targets_train, :, 1)
-    targets_val = reshape(targets_val, :, 1)
-    targets_test = reshape(targets_test, :, 1)
+    trainingInputs, trainingTargets = trainingDataset
+    testInputs, testTargets = testDataset
+    validationInputs, validationTargets = validationDataset
+    trainingTargets = reshape(trainingTargets, :, 1)
+    testTargets = reshape(testTargets, :, 1)
+    validationTargets = reshape(validationTargets, :, 1)
 
     # Llamamos a la función original con los nuevos argumentos
-    return trainClassANN(topology, (inputs_train, targets_train); validationDataset =(inputs_val, targets_val), testDataset=(inputs_test, targets_test), transferFunctions=transferFunctions, maxEpochs=maxEpochs, minLoss=minLoss, learningRate=learningRate, maxEpochsVal=maxEpochsVal)
+    return trainClassANN(topology, (trainingInputs, trainingTargets);
+    validationDataset =(validationInputs, validationTargets),
+    testDataset=(testInputs, testTargets), transferFunctions=transferFunctions, maxEpochs=maxEpochs,
+    minLoss=minLoss, learningRate=learningRate, maxEpochsVal=maxEpochsVal)
 end
