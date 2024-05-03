d5409d0f2c54c7ae3177dba92900acd54f750cde
iago Grandal <i.gdelrio@udc.es>
2024-05-03 17:43:23 +0200

    kneiughbours commented, with the rest running

--- src/main_obesity.jl
+++ src/main_obesity.jl
@@ -60,21 +60,11 @@ end
 
 # inputs format
 inputs_train = hcat(inputs_train...)
-println("Matriz",DataFrame(inputs_train, :auto))
-println("Tamaño matriz", size(inputs_train))
+# println("Matriz",DataFrame(inputs_train, :auto))
+# println("Tamaño matriz", size(inputs_train))
 
 # targets operations
 targets_train = [row[TARGETS] for row in eachrow(data)]
-
-
-
-
-
-
-# Define los hiperparámetros del modelo (ajústalos según tus necesidades)
-modelHyperparameters = Dict(
-    "n_neighbors" => 2,  # Ejemplo de topología de red neuronal
-)
 
 # Define el número de folds para la validación cruzada
 num_folds = 5
@@ -88,10 +78,74 @@ num_folds = 5
 # Cargar los índices desde el archivo
 crossValidationIndices = readdlm("crossValidationIndices.csv", ',', Int64)[:]
 
-println("crossValidationIndices: ", crossValidationIndices)
-println("Nº de índices: ", size(crossValidationIndices)) # Debería ser igual al número de instancias
+# println("crossValidationIndices: ", crossValidationIndices)
+# println("Nº de índices: ", size(crossValidationIndices)) # Debería ser igual al número de instancias
+
+# modelHyperparameters = Dict(
+#     "n_neighbors" => 2,  # Ejemplo de topología de red neuronal
+# )
+
+# # Aplica modelCrossValidation en la GPU
+# resultados = gpu(modelCrossValidation)(:KNeighborsClassifier, modelHyperparameters, inputs_train, targets_train, crossValidationIndices)
+# println(resultados)
+
+depths = [6, 7, 5, 8, 9, 10, 12, 15]
+for depth in depths
+    modelHyperparameters = Dict(
+        "max_depth" => depth
+    )
+    testAccuracy, testErrorRate, testRecall, testSpecificity, testPrecision, testNPV, testF1 = gpu(modelCrossValidation)(:DecisionTreeClassifier,
+    modelHyperparameters, inputs_train, targets_train, crossValidationIndices)
+
+    println("depth de esta ronda: \n", depth, "\n\n\n")
+    println("testAccuracy: \n", testAccuracy, "\n\n")
+    println("testErrorRate: \n", testErrorRate, "\n\n")
+    println("testRecall: \n", testRecall, "\n\n")
+    println("testSpecificity: \n", testSpecificity, "\n\n")
+    println("testPrecision: \n", testPrecision, "\n\n")
+    println("testNPV: \n", testNPV, "\n\n")
+    println("testF1: \n", testF1, "\n\n")
+end
 
 
-# Aplica modelCrossValidation en la GPU
-resultados = gpu(modelCrossValidation)(:KNeighborsClassifier, modelHyperparameters, inputs_train, targets_train, crossValidationIndices)
-println(resultados)
+allNeigbours = [2, 3, 4, 5, 6, 7, 8, 9]
+
+# for neighbours in allNeigbours
+#     modelHyperparameters = Dict(
+#         "n_neighbors" => neighbours,  # Ejemplo de topología de red neuronal
+#     )
+#     testAccuracy, testErrorRate, testRecall, testSpecificity, testPrecision, testNPV, testF1 = gpu(modelCrossValidation)(:ANN,
+#     modelHyperparameters, inputs_train, targets_train, crossValidationIndices)
+
+#     println("neighbours de esta ronda: \n", neighbours, "\n")
+#     println("testAccuracy: \n", testAccuracy, "\n\n")
+#     println("testErrorRate: \n", testErrorRate, "\n\n")
+#     println("testRecall: \n", testRecall, "\n\n")
+#     println("testSpecificity: \n", testSpecificity, "\n\n")
+#     println("testPrecision: \n", testPrecision, "\n\n")
+#     println("testNPV: \n", testNPV, "\n\n")
+#     println("testF1: \n", testF1, "\n\n\n")
+# end
+
+
+topologies = [[10, 15, 4, 6], [15, 3, 7, 5], [13, 8, 9, 11], [5, 7, 14], [8, 10, 4], [10, 6, 11, 5], [6, 14, 4]]
+
+for topology in topologies
+    modelHyperparameters = Dict(
+        "topology" => topology,
+        "learningRate" => 0.01,
+        "maxEpochs" => 1000,
+        "numExecutions" => 7,
+    )
+    testAccuracy, testErrorRate, testRecall, testSpecificity, testPrecision, testNPV, testF1 = gpu(modelCrossValidation)(:ANN,
+    modelHyperparameters, inputs_train, targets_train, crossValidationIndices)
+
+    println("topology de esta ronda: \n", topology, "\n\n\n")
+    println("testAccuracy: \n", testAccuracy, "\n\n")
+    println("testErrorRate: \n", testErrorRate, "\n\n")
+    println("testRecall: \n", testRecall, "\n\n")
+    println("testSpecificity: \n", testSpecificity, "\n\n")
+    println("testPrecision: \n", testPrecision, "\n\n")
+    println("testNPV: \n", testNPV, "\n\n")
+    println("testF1: \n", testF1, "\n\n\n")
+end
